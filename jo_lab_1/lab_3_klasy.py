03f912ba93972784cdcb3e88ba7b7bef48ccf32d
michaszo18 <michaszo18@gmail.com>
2021-01-08 20:22:00 +0100

    zad3

--- jo_lab_1/lab_3_klasy.py
+++ jo_lab_1/lab_3_klasy.py
@@ -1,9 +1,30 @@
-class Person:
-    name = "Jack"
+class Car:
+
+    def __init__(self, car_type, brand, model, engine):
+        self.car_type = car_type
+        self.brand = brand
+        self.model = model
+        self.engine = engine
+        self.doors = None
+        self.airbag = None
+        self.navigation = None
+        self.backup_camera = None
+        self.bluetooth = None
+        self.sport_driver = None
+
+    def set_doors(self, doors):
+        self.doors = doors
+
+    def set_airbag(self, airbag):
+        self.airbag = airbag
+
+    def set_navigation(self, navigation):
+        self.navigation = navigation
 
 
-print(Person.name)
+class SportCar(Car):
+    def __init__(self):
+        super(Car, self).__init__()
 
-i = 11;
 
-str(11)
+SportCar.set_airbag(3)
